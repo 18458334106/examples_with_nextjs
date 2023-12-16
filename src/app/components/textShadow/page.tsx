3528c025bd3abe1fdfc3481c3342eb4ec42f0703
19857191790 <2418671097@qq.com>
2023-12-16 14:15:31 +0800

    new

---
+++ src/app/components/textShadow/page.tsx
@@ -0,0 +1,9 @@
+import styles from './TextShadow.module.scss'
+
+export default function TextShadow(){
+    return (
+        <div className={`${styles.container} flexRowCenterAll`}>
+            <h1>show text effect</h1>
+        </div>
+    )
+}
